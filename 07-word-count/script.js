76586ac84048aef55a2725ac49416ebbc61d2492
bray-hiramis <johnbrianhiramis@gmail.com>
2025-07-07 13:33:32 +0800

    fix the bug in character and word count project

--- 07-word-count/script.js
+++ 07-word-count/script.js
@@ -10,13 +10,21 @@ const count = (words) => {
    const splitWords = words.value.split(" ");
    const filterWords = splitWords.filter((word) => word !== "");
    wordCount.textContent = filterWords.length;
-
+   
+   // Code block to limit the character count to 200 and the word count based on the max-char 200
    if (char >= maxChar) {
-      const sliceChar = words.value.slice(0, maxChar - 1);
-      words.value = sliceChar;
+      const sliceChar = words.value.slice(0, maxChar); // slice from index 0 to 200
+      words.value = sliceChar; // limits the input to 200
+      charCount.textContent = sliceChar.length; // shows the number of characters with max 200
+
+      // stops the word counting when the character count reached 200
+      wordCount.textContent = sliceChar.split(" ").filter((word) => word !== "").length;
+
       charCount.style.color = "red";
+      wordCount.style.color = "red";
    } else {
       charCount.style.color = "black";
+      wordCount.style.color = "black";
    };
 };
 
